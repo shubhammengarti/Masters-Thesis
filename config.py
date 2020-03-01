330f5b18a0265f68bb9b7176b797f888bd379ac5
shubhammengarti <shubhammengarti@gmail.com>
2020-03-01 15:51:40 +0000

    Master`s Thesis
     Pharma and Industry base Greenhouse and Weather station monitoring over Big Data and analysis

---
+++ config.py
@@ -0,0 +1,7 @@
+staging="file:///home/hduser/project/data/staging"
+input="file:///home/hduser/project/data/input"
+output="file:///home/hduser/project/data/output"
+mySqlUrl="jdbc:mysql://localhost:3306"
+jdbcJar="file:///home/hduser/project/data/mysql.jar"
+model="file:///home/hduser/project/model"
+download="file:///home/spark/Downloads"
